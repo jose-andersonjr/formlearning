acab484c6a30fc110cf379d19ce75ac89a6454bd
José Anderson <jose.andersonjr2@gmail.com>
2022-04-05 11:24:51 -0300

    Arquivos adicionados, código html feito e modificação do css em andamento

---
+++ modelformlearning/core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from modelformlearning.core import forms
+from modelformlearning.core import forms
+from modelformlearning.core.forms import FormularioPrincipal
+
+# Create your views here.
+def formulario(request):
+    form = FormularioPrincipal()
+    context = {
+        'form':form
+    }
+    template_name='formulario.html'
+    return render(request, template_name, context=context)
